51bbdfaa3f9750b75ef3bbb00f1be3eaf413d836
alyonaVet <a.v.alenka2015@gmail.com>
2024-10-18 21:33:43 +0600

    added GoogleOAuthProvider with clientId;

--- frontend-app/src/main.tsx
+++ frontend-app/src/main.tsx
@@ -1,9 +1,13 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App';
+import {GoogleOAuthProvider} from '@react-oauth/google';
+import {GOOGLE_CLIENT_ID} from './constants';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App/>
-  </StrictMode>,
+  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+    <StrictMode>
+      <App/>
+    </StrictMode>
+  </GoogleOAuthProvider>,
 );
