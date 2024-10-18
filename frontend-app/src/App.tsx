901bda63c1a60ad2852c3409e3ef9a5a76e4e261
alyonaVet <a.v.alenka2015@gmail.com>
2024-10-18 22:23:36 +0600

    added AppToolbar component, routes to register, login, NotFoundPage;

--- frontend-app/src/App.tsx
+++ frontend-app/src/App.tsx
@@ -1,10 +1,22 @@
 import './App.css';
+import AppToolbar from './UI/AppToolbar/AppToolbar';
+import {Route, Routes} from 'react-router-dom';
+import Register from './features/users/components/Register';
+import Login from './features/users/components/Login';
+import NotFoundPage from './UI/NotFoundPage/NotFoundPage';
 
 const App = () => {
 
   return (
     <>
-      Hello world
+      <header>
+        <AppToolbar/>
+      </header>
+      <Routes>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="*" element={<NotFoundPage/>}/>
+      </Routes>
     </>
   );
 };
