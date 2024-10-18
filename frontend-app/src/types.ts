72bf43c5f341314190a17b490770886fbdcdebe5
alyonaVet <a.v.alenka2015@gmail.com>
2024-10-18 22:20:27 +0600

    created types RegisterCredentials,RegisterCredentials, User, Errors;

---
+++ frontend-app/src/types.ts
@@ -0,0 +1,36 @@
+export interface RegisterCredentials {
+  email: string;
+  password: string;
+  displayName: string;
+  avatar: string | null;
+}
+
+export interface c {
+  email: string;
+  password: string;
+}
+
+export interface User {
+  _id: string;
+  email: string;
+  token: string;
+  role: string;
+  displayName: string;
+  avatar?: string;
+}
+
+export interface ValidationError {
+  errors: {
+    [key: string]: {
+      name: string;
+      message: string;
+    }
+  },
+  message: string;
+  name: string;
+  _message: string;
+}
+
+export interface GlobalError {
+  error: string;
+}
