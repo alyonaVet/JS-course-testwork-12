6a34e7965e56a7f8d61705e30c5a782f87bf564e
alyonaVet <a.v.alenka2015@gmail.com>
2024-10-18 22:19:42 +0600

    added BrowserRouter, Provider store, PersistGate;

--- frontend-app/src/main.tsx
+++ frontend-app/src/main.tsx
@@ -3,11 +3,21 @@ import {createRoot} from 'react-dom/client';
 import App from './App';
 import {GoogleOAuthProvider} from '@react-oauth/google';
 import {GOOGLE_CLIENT_ID} from './constants';
+import {PersistGate} from 'redux-persist/integration/react';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {persistor, store} from './app/store';
 
 createRoot(document.getElementById('root')!).render(
   <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
     <StrictMode>
-      <App/>
+      <BrowserRouter>
+        <Provider store={store}>
+          <PersistGate persistor={persistor}>
+            <App/>
+          </PersistGate>
+        </Provider>
+      </BrowserRouter>
     </StrictMode>
   </GoogleOAuthProvider>,
 );
