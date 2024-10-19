b47965741b7fa18ce818ccd3288596f4aa1079f1
alyonaVet <a.v.alenka2015@gmail.com>
2024-10-19 19:13:06 +0600

    refactor: dialog moved to component FullSizePhoto;

--- frontend-app/src/features/photos/components/PhotoCard.tsx
+++ frontend-app/src/features/photos/components/PhotoCard.tsx
@@ -1,30 +1,10 @@
 import React, {useState} from 'react';
-import {
-  Box, Button,
-  Card,
-  CardActionArea,
-  CardContent,
-  CardMedia,
-  Dialog, DialogActions, DialogContent,
-  IconButton,
-  Stack,
-  styled,
-  Typography
-} from '@mui/material';
+import {Box, Card, CardActionArea, CardContent, CardMedia, Stack, styled, Typography} from '@mui/material';
 import {apiURL} from '../../../constants';
 import {User} from '../../../types';
 import {LoadingButton} from '@mui/lab';
-import CloseIcon from '@mui/icons-material/Close';
 import {Link, useParams} from 'react-router-dom';
-
-const BootstrapDialog = styled(Dialog)(({theme}) => ({
-  '& .MuiDialogContent-root': {
-    padding: theme.spacing(2),
-  },
-  '& .MuiDialogActions-root': {
-    padding: theme.spacing(1),
-  },
-}));
+import FullSizePhoto from '../../../UI/Modal/FullSizePhoto';
 
 interface Props {
   id: string;
@@ -114,34 +94,10 @@ const PhotoCard: React.FC<Props> = ({
           </CardActionArea>
         </Card>
       </Box>
-      <BootstrapDialog
-        onClose={handleClose}
-        aria-labelledby="customized-dialog-title"
-        open={open}
-      >
-        <IconButton
-          aria-label="close"
-          onClick={handleClose}
-          sx={(theme) => ({
-            position: 'absolute',
-            right: 8,
-            top: 8,
-            color: theme.palette.grey[500],
-          })}
-        >
-          <CloseIcon/>
-        </IconButton>
-        <DialogContent>
-          <img src={cardImage} alt={title} style={{width: '80vh'}}/>
-        </DialogContent>
-        <DialogActions>
-          <Button autoFocus onClick={handleClose}>
-            Close
-          </Button>
-        </DialogActions>
-      </BootstrapDialog>
+      <FullSizePhoto handleClose={handleClose} cardImage={cardImage} title={title} open={open}/>
     </>
   );
 };
 
 export default PhotoCard;
+
