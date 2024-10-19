c940f9ddbb95499cf5a09c77f402eaf3dd5882bb
alyonaVet <a.v.alenka2015@gmail.com>
2024-10-19 19:00:00 +0600

    added fixtures;

--- api-app/fixtures.ts
+++ api-app/fixtures.ts
@@ -1,12 +1,14 @@
 import mongoose from 'mongoose';
 import config from './config';
 import User from './models/User';
+import Photo from './models/Photo';
 
 const run = async () => {
   await mongoose.connect(config.database);
   const db = mongoose.connection;
   try {
     await db.dropCollection('users');
+    await db.dropCollection('photos');
   } catch (error) {
     console.log('Skipping drop...');
   }
@@ -26,6 +28,28 @@ const run = async () => {
       displayName: 'John Doe',
       avatar: 'fixtures/person.png',
     }
+  );
+
+  await Photo.create({
+      user: admin,
+      title: 'Beautiful nature',
+      image: 'fixtures/nature-1.jpeg'
+    },
+    {
+      user: admin,
+      title: 'Lake',
+      image: 'fixtures/nature-4.png'
+    },
+    {
+      user: user1,
+      title: 'Sun shining',
+      image: 'fixtures/nature-2.jpeg'
+    },
+    {
+      user: user1,
+      title: 'Summer forest',
+      image: 'fixtures/nature-3.webp'
+    },
   );
 
   await db.close();
