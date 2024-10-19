1796fb92c427983883379268699f0c726283eb8f
alyonaVet <a.v.alenka2015@gmail.com>
2024-10-19 12:38:27 +0600

    added photosRouter;

--- api-app/index.ts
+++ api-app/index.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import config from './config';
 import usersRouter from './routers/users';
+import photosRouter from './routers/photos';
 
 const app = express();
 const port = 8000;
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(cors(config.corsOptions));
 app.use(express.static('public'));
 app.use('/users', usersRouter);
+app.use('/photos', photosRouter);
 
 const run = async () => {
   await mongoose.connect(config.database);
