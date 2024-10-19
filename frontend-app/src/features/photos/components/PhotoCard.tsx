72492954a8f47c0bd58398999e71b237bd2cf852
alyonaVet <a.v.alenka2015@gmail.com>
2024-10-19 16:55:39 +0600

    added Dialog by clicking on image; styled by BootstrapDialog;

--- frontend-app/src/features/photos/components/PhotoCard.tsx
+++ frontend-app/src/features/photos/components/PhotoCard.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
-import {Box, Card, CardActionArea, CardContent, CardMedia, Stack, styled, Typography} from '@mui/material';
+import React, {useState} from 'react';
+import {
+  Box, Button,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Dialog, DialogActions, DialogContent,
+  IconButton,
+  Stack,
+  styled,
+  Typography
+} from '@mui/material';
 import {apiURL} from '../../../constants';
 import {User} from '../../../types';
 import {LoadingButton} from '@mui/lab';
+import CloseIcon from '@mui/icons-material/Close';
+
+const BootstrapDialog = styled(Dialog)(({theme}) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
+}));
 
 interface Props {
   id: string;
@@ -11,7 +32,6 @@ interface Props {
   username: string;
   title: string;
   image: string | null;
-  onClick: () => void;
   onDelete: () => void;
   isDeleting: boolean;
 }
@@ -22,10 +42,19 @@ const PhotoCard: React.FC<Props> = ({
                                       username,
                                       title,
                                       image,
-                                      onClick,
                                       onDelete,
                                       isDeleting
                                     }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const ImageCardMedia = styled(CardMedia)({
     height: 0,
     paddingTop: '100%',
@@ -35,49 +64,80 @@ const PhotoCard: React.FC<Props> = ({
   const cardImage = image ? apiURL + '/' + image : '';
 
   return (
-    <Box sx={{mt: 3}}>
-      {user && (user.role === 'admin' || user._id === authorId) &&
-        (<Stack direction="row" justifyContent="space-between" mb={2}>
-            <LoadingButton
-              type="submit"
-              disabled={isDeleting}
-              loadingPosition="center"
-              variant="outlined"
-              color="error"
-              sx={{mr: 1}}
-              onClick={onDelete}
-            >
-              <span>Delete</span>
-            </LoadingButton>
-          </Stack>
-        )}
-      <Card sx={{
-        width: 250,
-        m: 1,
-        '&:hover': {
-          boxShadow: 6,
-          transform: 'scale(1.03)',
-        },
-        transition: 'transform 0.3s ease, box-shadow 0.3s ease',
-      }}>
-        <CardActionArea onClick={onClick}>
-          {image ? (
-            <ImageCardMedia image={cardImage}/>
-          ) : (
-            <Box sx={{height: 0, paddingTop: '100%', backgroundColor: '#fafafa'}}/>
+    <>
+      <Box sx={{mt: 3}}>
+        {user && (user.role === 'admin' || user._id === authorId) &&
+          (<Stack direction="row" justifyContent="space-between" mb={2}>
+              <LoadingButton
+                type="submit"
+                disabled={isDeleting}
+                loadingPosition="center"
+                variant="outlined"
+                color="error"
+                sx={{mr: 1}}
+                onClick={onDelete}
+              >
+                <span>Delete</span>
+              </LoadingButton>
+            </Stack>
           )}
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div" textAlign="center">
-              {title}
-            </Typography>
-            <Typography gutterBottom variant="h5" component="div" textAlign="center">
-              by {username}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    </Box>
-
+        <Card sx={{
+          width: 250,
+          m: 1,
+          '&:hover': {
+            boxShadow: 6,
+            transform: 'scale(1.03)',
+          },
+          transition: 'transform 0.3s ease, box-shadow 0.3s ease',
+        }}>
+          <CardActionArea>
+            {image ? (
+              <ImageCardMedia image={cardImage} onClick={handleClickOpen}/>
+            ) : (
+              <Box sx={{height: 0, paddingTop: '100%', backgroundColor: '#fafafa'}}/>
+            )}
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div" textAlign="center">
+                {title}
+              </Typography>
+              <Typography gutterBottom variant="h5" component="div" textAlign="center">
+                by {username}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Box>
+      <BootstrapDialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={(theme) => ({
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: theme.palette.grey[500],
+          })}
+        >
+          <CloseIcon/>
+        </IconButton>
+        <DialogContent dividers>
+          <Typography gutterBottom>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+            consectetur ac, vestibulum at eros.
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose}>
+            Close
+          </Button>
+        </DialogActions>
+      </BootstrapDialog>
+    </>
   );
 };
 
